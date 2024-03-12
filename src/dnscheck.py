04bbb78ba6857a7f711737f6f6fc5aa89ae4b847
Ludvik Jerabek <ljerabek@proofpoint.com>
2024-03-12 00:27:26 -0400

    Update

--- src/dnscheck.py
+++ src/dnscheck.py
@@ -1,9 +1,9 @@
 import argparse
 import csv
+import ipaddress
 import os
 import re
 import sys
-from glob import glob
 from typing import Union, Optional
 from dns import rdatatype, resolver, reversename
 from dns.name import Name
@@ -15,15 +15,26 @@ DATA_TYPE_SPF = 'SPF Data'
 DATA_TYPE_MX = 'MX Data'
 DATA_TYPE_RDNS = 'RDNS Data'
 
-custom_dns_server = '8.8.8.8'
 custom_resolver = resolver.Resolver()
-custom_resolver.nameservers = [custom_dns_server]
+
+def validate_file_path(file_path):
+    if not os.path.exists(file_path):
+        raise argparse.ArgumentTypeError(f"File '{file_path}' does not exist.")
+    return file_path
 
 
 def validate_xlsx_file(file_path):
     if not file_path.lower().endswith('.xlsx'):
         raise argparse.ArgumentTypeError("File must have a .xlsx extension.")
     return file_path
+
+
+def parse_ip_list(ip: str):
+    try:
+        ipaddress.ip_address(ip)
+        return ip
+    except ValueError as e:
+        raise argparse.ArgumentTypeError(f"Invalid IP address: {e}")
 
 
 def dns_lookup(qname: Union[Name, str], rdtype: Union[RdataType, str], pattern: Optional[re.Pattern] = None):
@@ -44,6 +55,8 @@ def dns_lookup(qname: Union[Name, str], rdtype: Union[RdataType, str], pattern: 
                 records.append(record_text)
     except Exception as e:
         records.append(str(e))
+
+    print(records)
     return records
 
 
@@ -56,9 +69,11 @@ def main():
                                      description="""Bulk DNS Lookup Tool.""",
                                      formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=80))
 
-    parser.add_argument('-i', '--input', metavar='<file>', dest="input_files",
-                        nargs='+', type=str, required=True,
-                        help='CSV file containing a list of domains')
+    parser.add_argument('-i', '--input', metavar='<file>', dest="input_file",
+                        type=validate_file_path, required=True, help='CSV file containing a list of domains')
+
+    parser.add_argument("--ns", metavar='8.8.8.8', dest="ns",
+                        nargs='+', type=parse_ip_list, help="List of DNS server addresses")
 
     parser.add_argument('--dmarc', action="store_true", dest="dmarc_flag",
                         help='DMARC record lookup')
@@ -77,12 +92,12 @@ def main():
 
     args = parser.parse_args()
 
-    file_names = [file for pattern in args.input_files for file in glob(pattern) if os.path.isfile(file)]
+    if args.input_file:
+        print("Input file:", args.input_file)
 
-    print("Files to be processed:")
-    for files in file_names:
-        print(files)
-    print()
+    if args.ns:
+        print('Nameserver:', args.ns)
+        custom_resolver.nameservers = args.ns
 
     dns_data = {}
 
@@ -90,36 +105,35 @@ def main():
     spf_pattern = re.compile(r'^v=spf', re.IGNORECASE)
     mx_pattern = re.compile(r'.', re.IGNORECASE)
 
-    for f in file_names:
-        with open(f, 'r', encoding='utf-8-sig') as input_file:
-            reader = csv.DictReader(input_file)
-            for line in reader:
-                host = line['Domain'].strip()
+    with open(args.input_file, 'r', encoding='utf-8-sig') as input_file:
+        reader = csv.DictReader(input_file)
+        for line in reader:
+            host = line['Domain'].strip()
 
-                if args.dmarc_flag:
-                    dns_data.setdefault(DATA_TYPE_DMARC, {'max_cols': 0, 'data': []})
-                    data = dns_lookup('_dmarc.{}'.format(host), 'TXT')
-                    dns_data[DATA_TYPE_DMARC]['max_cols'] = max(len(data), dns_data[DATA_TYPE_DMARC]['max_cols'])
-                    dns_data[DATA_TYPE_DMARC]['data'].append([host] + data)
+            if args.dmarc_flag:
+                dns_data.setdefault(DATA_TYPE_DMARC, {'max_cols': 0, 'data': []})
+                data = dns_lookup('_dmarc.{}'.format(host), 'TXT')
+                dns_data[DATA_TYPE_DMARC]['max_cols'] = max(len(data), dns_data[DATA_TYPE_DMARC]['max_cols'])
+                dns_data[DATA_TYPE_DMARC]['data'].append([host] + data)
 
-                if args.spf_flag:
-                    dns_data.setdefault(DATA_TYPE_SPF, {'max_cols': 0, 'data': []})
-                    data = dns_lookup(host, 'TXT', spf_pattern)
-                    dns_data[DATA_TYPE_SPF]['max_cols'] = max(len(data), dns_data[DATA_TYPE_SPF]['max_cols'])
-                    dns_data[DATA_TYPE_SPF]['data'].append([host] + data)
+            if args.spf_flag:
+                dns_data.setdefault(DATA_TYPE_SPF, {'max_cols': 0, 'data': []})
+                data = dns_lookup(host, 'TXT', spf_pattern)
+                dns_data[DATA_TYPE_SPF]['max_cols'] = max(len(data), dns_data[DATA_TYPE_SPF]['max_cols'])
+                dns_data[DATA_TYPE_SPF]['data'].append([host] + data)
 
-                if args.mx_flag:
-                    dns_data.setdefault(DATA_TYPE_MX, {'max_cols': 0, 'data': []})
-                    data = dns_lookup(host, 'MX')
-                    dns_data[DATA_TYPE_MX]['max_cols'] = max(len(data), dns_data[DATA_TYPE_MX]['max_cols'])
-                    dns_data[DATA_TYPE_MX]['data'].append([host] + data)
+            if args.mx_flag:
+                dns_data.setdefault(DATA_TYPE_MX, {'max_cols': 0, 'data': []})
+                data = dns_lookup(host, 'MX')
+                dns_data[DATA_TYPE_MX]['max_cols'] = max(len(data), dns_data[DATA_TYPE_MX]['max_cols'])
+                dns_data[DATA_TYPE_MX]['data'].append([host] + data)
 
-                if args.reverse_flag:
-                    dns_data.setdefault(DATA_TYPE_RDNS, {'max_cols': 0, 'data': []})
-                    reversed_ip = reversename.from_address(host)
-                    data = dns_lookup(reversed_ip, 'PTR')
-                    dns_data[DATA_TYPE_RDNS]['max_cols'] = max(len(data), dns_data[DATA_TYPE_RDNS]['max_cols'])
-                    dns_data[DATA_TYPE_RDNS]['data'].append([host] + data)
+            if args.reverse_flag:
+                dns_data.setdefault(DATA_TYPE_RDNS, {'max_cols': 0, 'data': []})
+                reversed_ip = reversename.from_address(host)
+                data = dns_lookup(reversed_ip, 'PTR')
+                dns_data[DATA_TYPE_RDNS]['max_cols'] = max(len(data), dns_data[DATA_TYPE_RDNS]['max_cols'])
+                dns_data[DATA_TYPE_RDNS]['data'].append([host] + data)
 
     workbook = xlsxwriter.Workbook(args.output_file)
 
